efc4fb6afb346e75d703ed38f0f9b3b1b4d497f9
Marcelo Santos <rocha.mer21@gmail.com>
2024-10-10 11:38:42 -0300

    fix alguns bugs

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -146,25 +146,6 @@ end
 return {
     {
         "neovim/nvim-lspconfig",
-        opts = {
-            servers = {
-                taplo = {
-                    keys = {
-                        {
-                            "K",
-                            function()
-                                if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
-                                    require("crates").show_popup()
-                                else
-                                    vim.lsp.buf.hover()
-                                end
-                            end,
-                            desc = "Show Crate Documentation",
-                        },
-                    },
-                },
-            },
-        },
     },
     {
         "mrcjkb/rustaceanvim",
